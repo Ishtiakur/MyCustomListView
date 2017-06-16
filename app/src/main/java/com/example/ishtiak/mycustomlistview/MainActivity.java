7f52645d6a3a7327a2a719dce12d545ba121c1e7
Ishtiak <ishtiak24014@gmail.com>
2017-06-17 00:03:07 +0600

    this update for floating action button

--- app/src/main/java/com/example/ishtiak/mycustomlistview/MainActivity.java
+++ app/src/main/java/com/example/ishtiak/mycustomlistview/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.ishtiak.mycustomlistview;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -43,6 +46,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ListView listView = (ListView) findViewById(R.id.lv);
+
+
+        //for folating action bar
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         context = this;
 
         //to increase array item
